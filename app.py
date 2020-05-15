710c9855454f33d94888692abbd009fdc9dbdc60
Ana <amr2343@columbia.edu>
2020-05-15 02:58:29 -0500

    added page templates

--- app.py
+++ app.py
@@ -14,11 +14,19 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
 
 @app.route("/1006")
 def test():
     return render_template("index.html")
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+@app.route("/assignments")
+def assign():
+    return render_template("assignments.html")
 
 #start the server
 if __name__ == "__main__":
